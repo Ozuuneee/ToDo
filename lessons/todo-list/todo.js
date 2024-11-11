5ded658f15f863e7ac4e027ae88e585ae2ed035c
Ozuuneee <ozunegaming@gmail.com>
2024-11-11 18:05:58 +0800

    todo-start

--- lessons/todo-list/todo.js
+++
@@ -1,8 +0,0 @@
-function getName() {
-  let yourname = prompt();
-  document.getElementById("output").innerHTML = yourname;
-}
-function deleteName() {
-  let disappear = "";
-  document.getElementById("output").innerHTML = disappear;
-}
